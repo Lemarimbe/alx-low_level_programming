34695b2944fec72f31694749823db18f6a698af2
Lemarimbe <lemarimbelydia@gmail.com>
2023-05-08 10:02:41 -0700

    copies memory area

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,16 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * File: holberton.h
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x06-pointers_arrays_strings directory.
- */
-
-#include <stddef.h>
-
+char *_memset(char *s, char b, unsigned int n);
 int _putchar(char c);
-void *_memset(void *s, int c, size_t n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
