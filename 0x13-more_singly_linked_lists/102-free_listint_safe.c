a4376d87102f9395dcb6e90f5519b76200307d15
Lemarimbe <lemarimbelydia@gmail.com>
2023-06-06 10:54:11 -0700

    another try

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,30 +1,40 @@
 #include "lists.h"
 #include <stdio.h>
-/**
- * print_listint_safe - prints the elements in linked list
- * @h: head of listint_t type
- *
- * Return: size_t, number of nodes. Exits with 98 on failure
- */
-size_t print_listint_safe(const listint_t *h)
-{
-	size_t n_nodes = 0;
-	const listint_t *temp_h = NULL, *loop_node= NULL;
+#include <stdlib.h>
 
-	temp_h = h;
-	if (!h)
-		exit(98);
+/**
+ * free_listint_safe - frees a listint_t list
+ * @h: double pointer to the head of listint_t type
+ *
+ * Return: size_t, the size of the list that was freed
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t size = 0;
+	listint_t *temp_h = *h, *loop_node = NULL;
+
+	if (*h == NULL)
+		return (0);
+
 	while (temp_h)
 	{
-		if (h->next == temp_h && n_nodes > 2)
+		if (*h == temp_h && size > 0)
 		{
 			loop_node = temp_h;
-			printf("loop node: [%p] %d\n", (void *)loop_node, loop_node->n);
-			exit(98);
+			break;
 		}
-		printf("[%p] %d\n", (void *)temp_h, temp_h->n);
-		temp_h = temp_h->next;
-		n_nodes++;
+		*h = temp_h->next;
+		free(temp_h);
+		temp_h = *h;
+		size++;
 	}
-	return (n_nodes);
+
+	if (loop_node != NULL)
+	{
+		size++;
+		*h = NULL;
+	}
+
+	return (size);
 }
+
