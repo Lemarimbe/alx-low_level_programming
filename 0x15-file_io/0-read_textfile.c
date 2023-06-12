b111f357c725208ea16401a5b5a096e169026153
Lemarimbe <lemarimbelydia@gmail.com>
2023-06-12 09:35:56 -0700

    fixing error handling issue

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,38 +1,47 @@
 #include "main.h"
+
 /**
  * read_textfile - reads a text file and prints it to the standard output
  * @filename: name of the file to be read
  * @letters: number of letters to read and print
- * Return: the number of letters printed, or 0 if it failed
+ * Return: the number of letters read and printed, or 0 if it failed
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	int i, y;
+	ssize_t bytes_read, bytes_written;
 	char *buffer;
 
-	if (!filename)
+	if (filename == NULL)
 		return (0);
+
 	fd = open(filename, O_RDONLY);
-	if (fd < 0)
+	if (fd == -1)
 		return (0);
-	buffer = malloc(sizeof(char) * letters);
-	if (!buffer)
-		return (0);
-	i = read(fd, buffer, letters);
-	if (i < 0)
+
+	buffer = malloc(sizeof(char) * (letters + 1));
+	if (buffer == NULL)
 	{
-		free(buffer);
+		close(fd);
 		return (0);
 	}
-	buffer[i] = '\0';
+
+	bytes_read = read(fd, buffer, letters);
+	if (bytes_read == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	buffer[bytes_read] = '\0';
 	close(fd);
-	y = write(STDOUT_FILENO, buffer, i);
-	if (y < 0)
-	{
-		free(buffer);
-		return (0);
-	}
+
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 	free(buffer);
-	return (y);
+
+	if (bytes_written != bytes_read)
+		return (0);
+
+	return (bytes_read);
 }
